d39f4c5641b2421fdf3e155efa52aca3fe2bfcc9
Kareem Zuwa <kareemzuwa@gmail.com>
2021-11-03 12:09:05 +0100

    galleri delete hamster works

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -7,16 +7,22 @@ import '../gallery/Gallery.css'
 const Gallery = () => {
     const dispatch = useDispatch()
     const hamsters = useSelector((state: RootState) => state.hamsters)
-    
+
     useEffect(() => {
 		async function sendRequest() {
             const response = await fetch('/hamsters')
-            const dataHamster = await response.json()
-            dispatch(actions.getHamsters(dataHamster))
+            const dataHamster = await response.json() 
+            dispatch(actions.getHamsters(dataHamster)) 
         }
         sendRequest()
 	}, [])
 
+    const deleteHamster = async function deleteHamster(id: string) {
+        fetch('/hamsters/' + id, { method: 'DELETE' }) 
+        .then(res => res.json()) 
+        .then(data => { console.log(data) }) 
+        .catch(err => console.log(err));
+    }
     const handleRemove = (hamsterId: string) => dispatch(actions.removeHamster(hamsterId))
 
     return (
@@ -31,7 +37,7 @@ const Gallery = () => {
                         <li key={hamster.id}> 
                         <img src={'/img/' + hamster.imgName} alt="hamster" width="100px"/>
                         <span>Namn: {hamster.name} </span>
-                        <button onClick={()=> handleRemove(hamster.id)}> Radera hamster </button>
+                        <button onClick={()=> {deleteHamster(hamster.id); handleRemove(hamster.id)}}> Radera hamster </button>
                         </li>
                     )): <h3> Väntar på galleri med hamsters </h3>}
                
